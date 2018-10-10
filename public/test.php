6a57f4018e00f4617800c76c5275b73bf180043d
wq455133 <wq455133@163.com>
2018-10-10 23:13:49 +0800

    curl

---
+++ public/test.php
@@ -0,0 +1,10 @@
+<?php
+
+// 类自动加载
+spl_autoload_register(function ($class) {
+    include dirname(__DIR__) . '/class/' . $class . '.class.php';
+});
+// echo 2233;die;
+$curl = new Curl();
+$res = $curl->https_post('http://dev.local.test/api.test.php', array('name' => 'wangqiang'));
+print_r($res);
