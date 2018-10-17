0d6286b7dc79e2be31a72733969a922a83bba098
wq455133 <wq455133@163.com>
2018-10-18 02:03:57 +0800

    update dir

--- public/support/helpers.php
+++ public/support/helpers.php
@@ -4,7 +4,7 @@ if (!function_exists('str_microtime')) {
     /**
      * 获取当前毫秒的时间
      */
-    function str_microtime()
+    function format_microtime()
     {
         list($t1, $t2) = explode(' ', microtime());
         return (float) sprintf('%.0f', (floatval($t1) + floatval($t2)) * 1000);
@@ -14,17 +14,20 @@ if (!function_exists('str_microtime')) {
 if (!function_exists('str_random')) {
     /**
      * 随机固定长度字符串
-     * @param string $length
+     * @param int $length
      * @param int $type
+     * @return string
      */
-    function str_random($length = 30, $type = 0)
+    function str_random($length = 30, $type = 1)
     {
         if ($type == 1) {
             $pool = '0123456789';
         } else if ($type == 2) {
             $pool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        } else {
+        } else if ($type == 3) {
             $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        } else {
+            return 1;
         }
         return substr(str_shuffle(str_repeat($pool, $length)), 0, $length);
     }
