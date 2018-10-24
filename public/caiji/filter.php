5fd148e342c14b5a8e50f93d10c6ddcf4e00cfbc
王强 <wangqiang@medlive.cn>
2018-10-24 09:37:14 +0800

    update page class

--- public/caiji/filter.php
+++ public/caiji/filter.php
@@ -1,18 +1,9 @@
 <?php
 
-$html = '我<article><p>我们是公司的分公司的风格</p></article><div>公司弗兰克的结果是浪费的恐惧感</div>';
-preg_match('/^(<\/*[a-z]*>)*我/i', $html, $matches);
+$html = '我<article class="blue" style="width: 10px;"><p style="width: 10px;">我们是公司的分公司的风格</p></article><div>公司弗兰克的结果是浪费的恐惧感</div>';
+preg_match('/^(<\/*[a-z]*[\s\S]*>)*我/i', $html, $matches);
 print_r($matches);die;
-//
-//$html = preg_replace('/<[\/\w]*>/', '', $html);
-//if (preg_match('/^我/', $html)) {
-//    echo 1;
-//} else {
-//    echo 0;
-//}
-
-var_dump(filter_me($html));
 
 function filter_me($html) {
-    return preg_match('/^(<\/*a-z>)*我/', $html);
+    return preg_match('/^(<\/*[a-z]*[\s\S]*>)*我/i', $html);
 }
