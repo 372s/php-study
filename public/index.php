406c0b8c6e97ad9a7910a815fb070a64187808d9
wq455133 <wq455133@163.com>
2018-11-24 17:22:29 +0800

    update

--- public/index.php
+++ public/index.php
@@ -12,6 +12,25 @@ $spl_class = spl_classes();
 // echo date('Y-m-d', '1542738806');die;
 
 
+// 将文本中的年份增加一年.
+$text = "April fools day is 04/01/2002\n";
+$text.= "Last christmas was 12/24/2001\n";
+
+echo preg_replace('|(\d{2}/\d{2}/)(\d{4})|', "$1$2+1", $text);die;
+// 回调函数
+function next_year($matches)
+{
+    // 通常: $matches[0]是完成的匹配
+    // $matches[1]是第一个捕获子组的匹配
+    // 以此类推
+    var_dump($matches);
+    return $matches[1].($matches[2]+1);
+}
+echo preg_replace_callback(
+    "|(\d{2}/\d{2}/)(\d{4})|",
+    "next_year",
+    $text);die;
+
 $className = '\Dir\Name';
 $className = ltrim($className, '\\');
 echo $className;
