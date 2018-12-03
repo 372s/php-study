67715447ad3b5432e5e7d9eb33270cfc13a76113
wq455133 <wq455133@163.com>
2018-12-03 18:36:04 +0800

    update

---
+++ public/caiji/360.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 360
+ */
+require_once __DIR__ . '/helpers.php';
+
+
+$urls = array(
+    'http://third.api.btime.com/News/shNews?from=so&cid=1&os=win&timestamp=12232323&c=domestic&_=1543833196999',
+
+);
