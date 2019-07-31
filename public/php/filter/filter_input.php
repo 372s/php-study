88e3367aee2e300908cbf7d16cfc047faf098537
wq455133 <wq455133@163.com>
2019-07-31 12:51:58 +0800

    update php

---
+++ public/php/filter/filter_input.php
@@ -0,0 +1,5 @@
+<?php
+// echo filter_input(INPUT_GET, 'var', FILTER_SANITIZE_STRING);die;
+// echo filter_input(INPUT_GET, 'var', FILTER_VALIDATE_URL);die;
+// echo filter_input(INPUT_GET, 'var', FILTER_VALIDATE_EMAIL);die;
+// var_dump(filter_input(INPUT_GET, 'var', FILTER_VALIDATE_INT));die;
