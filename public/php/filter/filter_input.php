d008377959d1415b0d40c26411b66ffd78cab01c
wq455133 <wq455133@163.com>
2019-07-31 12:52:18 +0800

    add

--- public/php/filter/filter_input.php
+++ public/php/filter/filter_input.php
@@ -3,3 +3,4 @@
 // echo filter_input(INPUT_GET, 'var', FILTER_VALIDATE_URL);die;
 // echo filter_input(INPUT_GET, 'var', FILTER_VALIDATE_EMAIL);die;
 // var_dump(filter_input(INPUT_GET, 'var', FILTER_VALIDATE_INT));die;
+
